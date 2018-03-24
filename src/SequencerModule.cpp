9129ff3c2bdc74b7d87f050fdd426f88e7d67024
squinkylabs <squinkylabs@users.noreply.github.com>
2018-03-24 16:47:19 -0700

    events

--- src/SequencerModule.cpp
+++ src/SequencerModule.cpp
@@ -1,4 +1,5 @@
 #ifdef _EXP
+#include <iostream>
 #include "Squinky.hpp"
 
 #include "WidgetComposite.h"
@@ -22,7 +23,8 @@ struct NoteDisplay : OpaqueWidget {
 
     void draw(NVGcontext *vg) override 
     {
-        nvgScale(vg, 2, 2);
+        // draw some squares for fun
+       // nvgScale(vg, 2, 2);
         nvgFillColor(vg, nvgRGBA(0xff, 0x00, 0x00, 0xff));
         nvgBeginPath(vg);
         nvgRect(vg, 50, 50, 30, 30);
@@ -33,6 +35,48 @@ struct NoteDisplay : OpaqueWidget {
         nvgRect(vg, 0, 0, 30, 30);
 		nvgFill(vg);
     }
+    void onMouseDown(EventMouseDown &e) override
+    {
+        OpaqueWidget::onMouseDown(e);
+        std::cout << "onMouseDown " << e.button << std::flush << std::endl;
+    }
+	void onMouseMove(EventMouseMove &e) override
+    {
+       //  std::cout << "onMouseMove " << std::flush << std::endl;
+	    OpaqueWidget::onMouseMove(e);
+    }
+	void onFocus(EventFocus &e) override
+    {
+         std::cout << "onFocus " << std::flush << std::endl;
+       // OpaqueWidget::onFocus(e);
+       e.consumed = true;
+    }
+	void onText(EventText &e) override
+    {
+        std::cout << "onText " <<  std::flush << std::endl;
+        OpaqueWidget::onText(e);
+    }
+
+	void onKey(EventKey &e) override
+    {
+        std::cout << "onKey " << e.key << std::flush << std::endl;
+        if (!e.consumed) {
+		    OpaqueWidget::onKey(e);
+	    }
+    }
+    void onMouseEnter(EventMouseEnter &e) override
+     {
+         std::cout << "nmouseenger "  << std::flush << std::endl;
+     }
+	/** Called when another widget begins responding to `onMouseMove` events */
+//	virtual void onMouseLeave(EventMouseLeave &e) {}
+	//virtual void onFocus(EventFocus &e) {}
+	void onDefocus(EventDefocus &e) override
+    
+     {
+        std::cout << "defoucs "  << std::flush << std::endl;
+        e.consumed=true;
+     }
 };
 
                                   //SequencerModule
