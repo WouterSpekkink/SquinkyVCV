d321a05857cf3f45e57bf5e95f412af411696afd
squinkylabs <squinkylabs@users.noreply.github.com>
2018-06-23 14:33:27 -0700

    -

--- test/testStochasticGrammar.cpp
+++ test/testStochasticGrammar.cpp
@@ -1,10 +1,28 @@
 
 #include "StochasticGrammar.h"
 
+#include <string>
+
 static void test0()
 {
     GKEY key;
+    GKEY outKeys[ProductionRuleKeys::bufferSize];
     for (key = sg_first; key <= sg_last; ++key) {
+        ProductionRuleKeys::breakDown(key, outKeys);
+        for (GKEY* p = outKeys; *p != sg_invalid; ++p) {
+            assert(*p >= sg_first);
+            assert(*p <= sg_last);
+        }
+
+        std::string s = ProductionRuleKeys::toString(key);
+        assert(!s.empty());
+        assert(s.length() < 256);
+
+        printf("key: %s\n", s.c_str());
+
+        int dur = ProductionRuleKeys::getDuration(key);
+        assert(dur > 0);
+        assert(dur <= PPQ * 8);
 
     }
 }
