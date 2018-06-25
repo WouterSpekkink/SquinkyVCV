20e255bafcb643a7cf57b458da44b9d0986d85e2
squinkylabs <squinkylabs@users.noreply.github.com>
2018-06-24 22:10:05 -0700

    -

--- dsp/filters/GraphicEq.h
+++ dsp/filters/GraphicEq.h
@@ -18,22 +18,22 @@ private:
 // todo: refactor
 inline GraphicEq::GraphicEq()
 {
-
-    const float bw = .5;
+    // .5, 1 stage is 78..128. 2stage 164 273 / 
+    // .8  67..148 /  63..314  / 72..456
+    const float bw = .8f;
     const float baseFreq = 100.f / 44100.f;
     for (int i = 0; i < numStages; ++i) {
         params[i].setMode(StateVariableFilterParams<float>::Mode::BandPass);
         params[i].setFreq(baseFreq * (i + 1));
+        params[i].setNormalizedBandwidth(bw);
     }
 }
 
 inline float GraphicEq::run(float input)
 {
-    float in = input;
     float out = 0;
     for (int i = 0; i < numStages; ++i) {
-        out = StateVariableFilter<float>::run(in, states[i], params[i]);
-        in = out;
+        out += StateVariableFilter<float>::run(input, states[i], params[i]);
     }
     return out;
 }
