be1f27647910bd7e03918eb483e476be8b7fb50b
squinkylabs <squinkylabs@users.noreply.github.com>
2018-07-06 14:15:55 -0700

    -

--- test/testFilter.cpp
+++ test/testFilter.cpp
@@ -8,10 +8,9 @@
 #include "Analyzer.h"
 #include "asserts.h"
 
-// 3000 -> 3091 in regular mode
-// 3066 for accurate (same for double
 static void testPeak(std::function<float(float)> filter, float sampleRate, float expectedMax, float percentTolerance)
 {
+
     const int numSamples = 64 * 1024;
     FFTDataCpx x(numSamples);
     Analyzer::getFreqResponse(x, filter);
@@ -19,47 +18,44 @@ static void testPeak(std::function<float(float)> filter, float sampleRate, float
     int maxBin = Analyzer::getMax(x);
     float maxFreq = (FFT::bin2Freq(maxBin, 44100, numSamples) +
         FFT::bin2Freq(maxBin + 1, 44100, numSamples)) / 2;
-    printf("testPeak max bin = %d, freq=%f expected =%f\n", maxBin, maxFreq, expectedMax);
 
     const float delta = expectedMax * percentTolerance / 100.f ;      // One percent accuracy
+
     assertClose(maxFreq, expectedMax, delta);
-    
+
 }
 
+template <typename T>
 static void testBandpass(float Fc, float tolerancePercent)
 {
-    StateVariableFilterState<float> state;
-    StateVariableFilterParams<float> params;
+    StateVariableFilterState<T> state;
+    StateVariableFilterParams<T> params;
 
-   // const float Fc = 100;
     const float sampleRate = 44100;
 
-    params.setMode(params.Mode::BandPass);
+    params.setMode(StateVariableFilterParams<T>::Mode::BandPass);
     params.setFreq(Fc / sampleRate);
-    params.setNormalizedBandwidth(.7f);
+    params.setQ(3);
 
     std::function<float(float)> filter = [&state, &params](float x) {
-        auto y = StateVariableFilter<float>::run(x, state, params);
-       // printf("filter(%f) ret (%f)\n", x, y);
-        return y;
+        auto y = StateVariableFilter<T>::run(x, state, params);
+        return float(y);
     };
     testPeak(filter, sampleRate, Fc, tolerancePercent);
 }
 
+template <typename T>
 static void test1()
 {
-    printf("why are these tests failing now with deltas of 1 \n"); // 10 .. 3000 passed with 1 before
-    testBandpass(10, 10);
-    testBandpass(100, 2);
-    testBandpass(1000, 50);
-    testBandpass(3000, 360);
+    testBandpass<T>(10, 10);
+    testBandpass<T>(100, 2);
+    testBandpass<T>(1000, 3);
+    testBandpass<T>(3000, 12);
+    testBandpass<T>(6000, 35);      // This filter just gets inaccurate at high freq - don't know why
 }
 
 
 void testFilter()
 {
-    printf("todo: finish test filters\n");
-
-    test1();
-
+    test1<float>();
 }
