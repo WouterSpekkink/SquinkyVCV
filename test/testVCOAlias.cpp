16fa0a0b59d8cc7bc5d68c51a2fa5ca5edbc5dca
squinkylabs <squinkylabs@users.noreply.github.com>
2018-07-31 22:58:34 -0700

    -

--- test/testVCOAlias.cpp
+++ test/testVCOAlias.cpp
@@ -18,15 +18,48 @@ public:
     float maxAliasFreq;
 };
 
+/*
+
+Next: examine the spectrum. make sure all freq in spectrum are signal or alias
+*/
 
 
-void testAlias(std::function<float()> func)
+std::pair< std::vector<double>, std::vector<double>> getFrequencies(float fundamental, float sampleRate)
+{
+    std::vector<double> harmonics;
+    std::vector<double> alias;
+    const float nyquist = sampleRate / 2;
+    bool done = false;
+    for (int i = 1; !done; ++i) {
+        double freq = fundamental * i;
+        if (freq < nyquist) {
+            harmonics.push_back(freq);
+        } else {
+            double over = freq - nyquist;
+            if (over < nyquist) {
+                freq = nyquist - over;
+                alias.push_back(freq);
+            } else {
+                done = true;
+            }
+        }
+    }
+    return std::pair< std::vector<double>, std::vector<double>>(harmonics, alias);
+}
+
+
+void testAlias(std::function<float()> func, float fundamental)
 {
     FFTDataCpx spectrum(numSamples);
     Analyzer::getSpectrum(spectrum, false, func);
     Analyzer::getAndPrintFeatures(spectrum, 3, sampleRate);
 
 
+    auto x = getFrequencies(fundamental, sampleRate);
+    for (double h : x.first)
+        printf("harmonic at %.2f\n", h);
+    for (double a : x.second)
+        printf("alias at %.2f\n", a);
     // measure spectrum with not
 }
 
@@ -43,8 +76,9 @@ void testRawSaw()
     SawOscillator<float, false>::setFrequency(params, normalizedFreq);
     testAlias([&state, &params]() {
         return 30 * SawOscillator<float, false>::runSaw(state, params);
-        });
+        }, freq);
 
+    
 
     //test it
 }
