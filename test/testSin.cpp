0c55942278264dabcfd07d7702f680b9c2784ba4
squinkylabs <squinkylabs@users.noreply.github.com>
2018-08-15 18:38:13 -0700

    -

--- test/testSin.cpp
+++ test/testSin.cpp
@@ -4,7 +4,8 @@
 
 #include <functional>
 
-const static int numSamples = 256 * 1024;
+const static int numSamples = 16 * 256 * 1024;
+//const static int numSamples = 256;
 const double sampleRate = 44100.0;
 const double sampleTime = 1.0 / sampleRate;
 
@@ -17,7 +18,7 @@ static void testOsc(const std::string& title, double freq, std::function<float(v
     FFTDataCpx spectrum(numSamples);
     Analyzer::getSpectrum(spectrum, false, osc);
 
-   // Analyzer::getAndPrintFeatures(spectrum, 3, sampleRate, -140);
+    Analyzer::getAndPrintFeatures(spectrum, 3, sampleRate, -180);
 
     double signal = 0;
     double noise = 0;
@@ -74,9 +75,147 @@ static void testPhaseAcc2()
         });
 }
 
+template <typename T>
+class Osc2
+{
+public:
+    void setFreq(float t)
+    {
+        assert(t > 0 && t < .51);
+
+        // orig paper
+     //   tapWeight = t * (1.0 / AudioMath::Pi);
+      //  tapWeight = t * 2;
+
+        
+        const T omega = 2.0 * AudioMath::Pi * t;
+        tapWeight = 2 * sin(omega / 2.0);
+
+        // from kvre
+        //e= 2*sin(omega/2.0); // omega = 2*pi*freq/sr;
+    }
+    float run()
+    {
+        const T x = zX - tapWeight * zY;
+
+        // orig paper
+    //    const T y = (tapWeight * zX) + (1 - tapWeight * tapWeight) * zY;
+
+        // smith (seems whack?) but seems to work as well as the above
+        const T y = (tapWeight * x) + zY;
+
+        zX = x;
+        zY = y;
+        return float(zX);
+    }
+
+    const double k = sqrt(2);
+
+    T zX = 1.200049 / k;
+    T zY = .747444 / k;
+    T tapWeight = .1;
+};
+
+
+template <typename T>
+class Osc3
+{
+public:
+    void setFreq(float t)
+    {
+        assert(t > 0 && t < .51);
+        //   tapWeight = t * (1.0 / AudioMath::Pi);
+        tapWeight = 2 * sin(t);
+    }
+    float run()
+    {
+        const T x = zX + tapWeight * zY;
+        const T y = -(tapWeight * x) + zY;
+
+        zX = x;
+        zY = y;
+        return float(zX);
+    }
+
+ //   const double k = sqrt(2);
+
+    T zX = 1;
+    T zY = 1;
+    T tapWeight = .1;
+};
+
+
+static void testOsc2()
+{
+   const double f = Analyzer::makeEvenPeriod(testFreq, sampleRate, numSamples);
+   // const double f = sampleRate / 2;
+
+   printf("trying osc 2 at %f\n", f);
+    std::string title = "osc2";
+    Osc2<double> osc;
+    osc.setFreq(float(f * sampleTime));
+    for (int i = 0; i < 1000000; ++i) {
+        osc.run();
+    }
+
+    testOsc(title, f, [&osc]() {
+        return osc.run();
+        });
+}
+
+
+static void testOsc3()
+{
+     const double f = Analyzer::makeEvenPeriod(testFreq, sampleRate, numSamples);
+   // const double f = sampleRate / 2;
+
+    std::string title = "osc3";
+    Osc3<double> osc;
+    osc.setFreq(float(f * sampleTime));
+    for (int i = 0; i < 1000000; ++i) {
+        osc.run();
+    }
+
+    testOsc(title, f, [&osc]() {
+        return osc.run();
+        });
+}
+
+static void testOsc2Amp_sub(double freq)
+{
+    const int bufSize = 16 * 1024 * 16;
+    Osc2<double> osc;
+    osc.setFreq(float(freq * sampleTime));
+
+    for (int i = 0; i < 1000000; ++i) {
+        osc.run();
+    }
+
+
+    std::vector<float> data;
+    data.resize(bufSize);
+    for (int i = 0; i < bufSize; ++i) {
+        data[i] = osc.run();
+    }
+    auto minMax = AudioMath::getMinMax<float>(data.data(), bufSize);
+    printf("f= %.2f min/max = %f, %f. z = %f, %f\n", freq, minMax.first, minMax.second, osc.zX, osc.zY);
+    printf("sqr2 = %f\n", sqrt(2.0));
+}
+
+static void testOsc2Amp()
+{
+    testOsc2Amp_sub(1111);
+    testOsc2Amp_sub(40);
+    testOsc2Amp_sub(400);
+    testOsc2Amp_sub(4123);
+}
+
 void testSin()
 {
-    testPhaseAcc<float>();
-    testPhaseAcc<double>();
-    testPhaseAcc2();
+   // testPhaseAcc<float>();
+  //  testPhaseAcc<double>();
+   testPhaseAcc2();
+  // testOsc2();
+   // testOsc3();
+   // testOsc2Amp();
 }
